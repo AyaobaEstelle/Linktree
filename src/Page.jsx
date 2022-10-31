5386e12178608cb17b98975d147e1a5b66385303
AyaobaEstelle <abolarinesther98@gmail.com>
2022-10-31 21:13:01 +0100

    first commit

---
+++ src/Page.jsx
@@ -0,0 +1,37 @@
+// import icon from "../icon"
+
+const Page = () => {
+    return (
+        <div className="wrap">
+            <div className="page">
+                <div className="content">
+              <div className="profile">
+               <img id="profile_img" src="" alt="" />
+               <img src="" alt="" />
+              <h3 id="twitter">Esther Abolarin</h3>
+              <h2 id="slack">slack username: Ayaoba </h2>
+           </div>
+           <div className="section">
+             <div className="buttons">
+              <a href="https://training.zuri.team/" type="button" id="btn_zuri">Twitter</a>
+              <a href="http://books.zuri.team" type="button" id="books">zuri Team</a>
+              <a href="http://books.zuri.team" type="button" id="books">Zuri Books</a>
+              <a href="https://books.zuri.team" type="button" id="book_python">Python Books</a>
+              <a href="https://background.zuri.team" type="button" id="pitch">Background Check for Coders</a>
+              <a href="https://books.zuri.team/design-rules" type="button" id="book_design">Design Books</a>
+             </div>
+             </div>
+           </div>
+           </div>
+           <div className="socials">
+            <img src="" alt="" />
+            <img src="" alt="" />
+           </div>
+           <div className="footer">
+           <img src="" alt="" />
+           </div>
+        </div>
+    );
+}
+ 
+export default Page;
